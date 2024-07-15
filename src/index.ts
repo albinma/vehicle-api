f6c7b235f1dbf548e880f968710cb2ae01ea9414
Albin Ma <albinma@gmail.com>
2024-07-15 18:23:30 -0400

    feat: add cors and helmet

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import cors from '@elysiajs/cors';
 import { Elysia } from 'elysia';
+import { helmet } from 'elysia-helmet';
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+const app = new Elysia()
+	.use(cors())
+	.use(
+		helmet({
+			// This is a backend api only - no need for CSP
+			contentSecurityPolicy: false,
+		})
+	)
+	.get('/', () => 'Hello Elysia')
+	.listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
